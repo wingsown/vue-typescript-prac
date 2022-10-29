ad2b283c8efdb9937afb2d47059813c8b4c9c36b
Wilson <wj.buena@gmail.com>
2022-10-29 10:01:20 +0800

    Options API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="app">
-    Hello
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Jeffrey')">Change Name</button>
+    <button @click="changeAge(30)">Change Age</button>
   </div>
 </template>
 
@@ -12,6 +14,23 @@ export default defineComponent({
   name: 'App',
   components: {
     
+  },
+  data() {
+    return {
+      name: 'Wilson',
+      age: 27 as number | string
+    }
+  },
+
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: number | string) {
+      this.age = age
+      return age
+    }
   }
 });
 </script>
