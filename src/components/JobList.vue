07ad84b8c2e763c6b1c46fe85691d5e9de26d847
Wilson <wj.buena@gmail.com>
2023-05-14 10:07:48 +0800

    use of computed properties and add final styles

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="job-list">
         <p>Ordered by {{ order }} </p>
-        <ul>
-            <li v-for="job in jobs" :key="job.id">
+        <transition-group name="list" tag="ul">
+            <li v-for="job in orderedJobs" :key="job.id">
                 <h2>{{ job.title }} in {{ job.location }}</h2>
                 <div class="salary">
+                    <img src="../assets/rupee.svg" alt="rupee icon">
                     <p>{{ job.salary }} rupees</p>
                 </div>
                 <div class="description">
@@ -13,13 +14,13 @@
                     </p>
                 </div>
             </li>
-        </ul>
+        </transition-group>
     </div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
 import Job from '@/types/Job'
 import OrderTerm from '@/types/OrderTerm'
 
@@ -33,6 +34,15 @@ export default defineComponent({
             required: true,
             type: String as PropType<OrderTerm>
         }
+    },
+    setup(props) {
+        const orderedJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ? 1 : -1
+            })
+        })
+
+        return { orderedJobs }
     }
 })
 </script>
@@ -67,5 +77,9 @@ export default defineComponent({
     color: #17bf66;
     font-weight: bold;
     margin: 10px 4px;
+}
+
+.list-move {
+    transition: all 1s;
 }
 </style>
